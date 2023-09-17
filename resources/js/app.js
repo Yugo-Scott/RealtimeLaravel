54d29394b2572327dd3aa8e1dc9132b8502a678c
Yugo <satoyugo1022@gmail.com>
2023-09-18 00:23:37 +0900

    authenticatedしたユーザにしかbroadcastできない設定した

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,18 @@
 import './bootstrap';
 
-Echo.channel("notifications").listen(
-    "UserSessionChanged",
-    (e) => {
-        //e contains information that are being sent when the user session changes
-        console.log(e);
-        console.log(e.message);
-        console.log(e.type);
-        const notificationElement = document.getElementById("notification");
-        console.log(notificationElement)
+Echo.private("notifications").listen("UserSessionChanged", (e) => {
+    //e contains information that are being sent when the user session changes
+    console.log(e);
+    console.log(e.message);
+    console.log(e.type);
+    const notificationElement = document.getElementById("notification");
+    console.log(notificationElement);
 
-        notificationElement.innerHTML = e.message;
+    notificationElement.innerHTML = e.message;
 
-        notificationElement.classList.remove('invisible');
-        notificationElement.classList.remove('alert-success');
-        notificationElement.classList.remove('alert-danger');
+    notificationElement.classList.remove("invisible");
+    notificationElement.classList.remove("alert-success");
+    notificationElement.classList.remove("alert-danger");
 
-        notificationElement.classList.add(`alert-${e.type}`);
-    }
-);
+    notificationElement.classList.add(`alert-${e.type}`);
+});
